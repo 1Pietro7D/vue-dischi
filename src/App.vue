338475ee2d34f4bd211be5e8347fb21d8430a76c
PietroDanieli <1pie7d@gmail.com>
2022-09-27 05:36:08 +0200

    import my personal fold style and install google-Api and bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div>ciao</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "@/assets/style/generics.scss";
 </style>
+
+<!--
+@import "@/assets/style/generics.scss";
+@import "@/assets/style/variables.scss";
+@import "@/assets/style/position.scss"; 
+@import "@/assets/style/display.scss";
+@import "@/assets/style/flex.scss";
+@import "@/assets/style/grid.scss"; 
+-->
